849226ac0be5f790ea17c18ffbb98b296f606806
zl <zl03jsj@163.com>
2021-10-27 15:57:25 +0800

    output failed string to stderr

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config: Config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
