2b7c0b9e12ece9a800c1a68c608f0c7a4bc2f145
zl <zl03jsj@163.com>
2021-10-27 11:37:00 +0800

    read commandline args

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,30 @@
-#![allow(unused_variables)]
-#![allow(unreachable_patterns)]
 #![allow(unused_imports)]
-#![allow(dead_code)]
-#![allow(unreachable_code)]
-#![allow(unused_must_use)]
 
-mod guess;
-mod syntax;
-mod restaurant;
-mod dtd_types;
-mod exception;
-mod generics;
+use std::{env, fs};
+use std::fs::File;
 
-use crate::{
-    guess::game,
-    syntax::{r#struct, r#enum, r#match},
-    dtd_types::{vector, string, hash_map},
-};
+
+struct Config {
+    query: String,
+    filename: String,
+}
 
 fn main() {
-    generics::test_number_add();
-    generics::test_lifetime();
+    let args: Vec<String> = env::args().collect();
 
-    generics::test_larget_number();
-    generics::test_point();
-    generics::r#trait::test_trait();
-    exception::read_username_from_file(None);
-    exception::test_exception(false);
+    let (query, filename) = parse_config(&args);
 
-    exception::test_unwarp_or_else();
-    hash_map::test_map();
-    string::test_string();
-    vector::test_vector();
-    game::start();
-    r#enum::test_call();
-    r#enum::test_ip();
-    r#struct::rectangle_area();
-    r#match::test_match();
-    restaurant::run::eat_at_restaurant();
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let content: String = fs::read_to_string(filename).expect("reading file content error");
+
+    println!("file content is :\n{}", &content);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
