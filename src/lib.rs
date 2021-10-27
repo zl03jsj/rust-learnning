1aeadcb23417bc54cbd86a690fac91c9d023e0ff
zl <zl03jsj@163.com>
2021-10-27 14:51:03 +0800

    case insensitive

--- src/lib.rs
+++ src/lib.rs
@@ -60,7 +60,7 @@ mod tests {
         restaurant::run::eat_at_restaurant();
     }
 
-    use super::*;
+    use super::mini_grep::{search, search_case_insensitive};
 
     #[test]
     fn one_ok_result() {
@@ -71,7 +71,7 @@ safe, fast, productive.
 Pick three.";
         assert_eq!(
             vec!["safe, fast, productive."],
-            mini_grep::search(query, contents)
+            search(query, contents)
         );
     }
 
@@ -84,7 +84,37 @@ safe, fast, productive.
 Pick three.";
         assert_ne!(
             vec!["not contains this string"],
-            mini_grep::search(query, contents)
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
         );
     }
 }
@@ -127,6 +157,18 @@ pub mod mini_grep {
         let mut results = Vec::new();
         for line in contents.lines() {
             if line.contains(query) {
+                results.push(line);
+            }
+        }
+        results
+    }
+
+    pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+        let query = query.to_lowercase();
+        let mut results = Vec::new();
+
+        for line in contents.lines() {
+            if line.to_lowercase().contains(&query) {
                 results.push(line);
             }
         }
